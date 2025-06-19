6629200ed6e5a1652742c248dc643d7a82154bc2
monish durga <monishdurga077@gmail.com>
2025-06-19 19:22:05 +0530

    assignment_9

---
+++ assignment9.py
@@ -0,0 +1,21 @@
+# question 1
+# for i in range (1,6):
+#  i = 6-i
+#  print(i)
+
+#question 2
+for i in range (6,0,-1):
+ print(i)
+
+# #question 3
+# names = []
+# for i in range (3):
+#  name = input(f"enter the name{i+1}: ")
+#  names.append(name)
+# print("the list of names : ", names)
+
+
+ #question 7
+#  name = input("enter the name : ")
+#  for character in name:
+#   print(character)
